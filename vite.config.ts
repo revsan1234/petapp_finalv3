60d269a831991a0cb95308f00ab14b7c41594f01
revsan1234 <revasanchez@earthlink.net>
2026-01-09 09:34:13 -0700

    more 9:30 AM

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
+    emptyOutDir: true
   },
   server: {
     port: 3000,
