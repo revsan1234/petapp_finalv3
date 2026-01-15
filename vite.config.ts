83a201e44d5ab7dc5fc786447f7183f26ef383c3
revsan1234 <revasanchez@earthlink.net>
2026-01-15 07:59:13 -0700

    more ????  8:00

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
   },
   build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './'),
-    },
-  },
+    outDir: 'dist',
+    sourcemap: false
+  }
 })
